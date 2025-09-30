f9b08aca1bc0823acdaa71d53620ac34e83d0f0a
Danik <mr.granddd@gmail.com>
2025-09-30 16:08:27 +0300

    перезалив

--- obj/Debug/net9.0/Task-6.AssemblyInfo.cs
+++ obj/Debug/net9.0/Task-6.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Task-6")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f56c02a325dd8bdc0d642a6bf05fc18f6668a202")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e580c6c923dda11bb58d22158544776e4208f45")]
 [assembly: System.Reflection.AssemblyProductAttribute("Task-6")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Task-6")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
